610df6b5b0adffc7d9248063e440383ef826b7e1
流光丶 <harparker@outlook.com>
2025-10-29 15:08:31 +0800

    Improved readability

--- main.c
+++ main.c
@@ -10,20 +10,29 @@ int main(void) {
     int i, j, k;
     int cnt = 0;
     i = a;
-    while ( i <= a + 3 ) {
+    while ( i <= a + 3 )
+    {
         j = a;
-        while ( j <= a + 3 ) {
+        while ( j <= a + 3 )
+        {
             k = a;
-            while ( k <= a + 3 ) {
-                if ( i != j ) {
-                    if ( i != k ) {
-                        if ( j != k ) {
+            while ( k <= a + 3 )
+            {
+                if ( i != j )
+                {
+                    if ( i != k )
+                    {
+                        if ( j != k )
+                        {
                             cnt++;
                             printf("%d%d%d", i, j, k);
-                            if ( cnt == 6 ) {
+                            if ( cnt == 6 )
+                            {
                                 printf("\n");
                                 cnt = 0;
-                            } else {
+                            }
+                            else
+                            {
                                 printf(" ");
                             }
                         }
