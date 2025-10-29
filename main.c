f1b2ffd9f3f9a2de12a5c0d9a51e848c3362860b
流光丶 <harparker@outlook.com>
2025-10-29 14:48:55 +0800

    Reconstructed the framework

--- main.c
+++ main.c
@@ -8,9 +8,28 @@ int main(void) {
         return 1;
     }
     int i, j, k;
-    i = a + 1;
-    j = a + 2;
-    k = a + 3;
-    printf("%d %d %d %d", a, i, j, k);
+    int cnt = 0;
+    while ( i <= a + 3 ) {
+        j = a;
+        while ( j <= a + 3 ) {
+            k = a;
+            while ( k <= a + 3 ) {
+                if ( i != j ) {
+                    if ( i != k ) {
+                        if ( j != k ) {
+                            cnt++;
+                            printf("%d%d%d", i, j, k);
+                            if ( cnt == 6 ) {
+                                printf("\n");
+                                cnt = 0;
+                            } else {
+                                printf(" ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
